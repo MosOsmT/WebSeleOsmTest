143d210abd7aba990a06e01924d48c42789110c9
Omprakash <omprakash.medhe@gmail.com>
2019-01-19 23:40:25 +0530

    Initial commit

---
+++ src/test/java/com/testcases/LoginTest.java
@@ -0,0 +1,33 @@
+package com.testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void setUp(){
+		
+		driver=new ChromeDriver();		
+	}
+	
+	@Test
+	public void doLogin(){
+		
+		driver.get("http://gmail.com/");
+		driver.findElement(By.id("Email")).sendKeys("osmtest");
+		driver.findElement(By.id("Passwrd")).sendKeys("yfuy");
+	}
+	@AfterSuite
+	public void tearDown(){
+		
+		driver.quit();
+	}
+
+}
